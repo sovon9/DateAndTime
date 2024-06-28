00cf5f2db29b66d31c054f76882ae09e8f38a0f8
sovon singha <sovonsingha9@gmail.com>
2024-06-28 12:02:42 +0530

    commit-1

---
+++ src/main/java/java8/DateAndTime/LocalDateTest.java
@@ -0,0 +1,14 @@
+package java8.DateAndTime;
+
+import java.time.LocalDate;
+
+public class LocalDateTest {
+
+	public static void main(String[] args) {
+
+		 LocalDate date = LocalDate.now();
+		 System.out.println("current date: "+date);
+		
+	}
+
+}
